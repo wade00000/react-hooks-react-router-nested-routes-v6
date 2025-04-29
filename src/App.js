c63878000aa648b59fb123d2ec80a596d568d36c
WADE <MEANTOINE12@GMAIL.COM>
2025-04-29 11:54:24 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import NavBar from "./components/NavBar";
+import { useEffect,useState } from "react";
+import { Outlet } from "react-router-dom";
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() =>{
+      fetch("http://localhost:4000/users")
+      .then(r => r.json())
+      .then(data => setUsers(data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
     <>
       <header>
-        
+        <NavBar/>
       </header>
+      <Outlet context={users}/>
     </>
   );
 };
--- src/routes.js
+++ src/routes.js
@@ -3,28 +3,36 @@ import About from "./pages/About";
 import Login from "./pages/Login";
 import UserProfile from "./pages/UserProfile";
 import ErrorPage from "./pages/ErrorPage";
+import App from "./App";
 
 const routes = [
   {
     path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />
-  }, 
-  {
-    path: "/about",
-    element: <About />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/login",
-    element: <Login />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/profile/:id",
-    element: <UserProfile />,
-    errorElement: <ErrorPage />
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children:[
+      {
+        path: "/",
+        element: <Home />,
+        children:[
+          {
+            path: "/profile/:id",
+            element: <UserProfile />,
+          }
+        ]        
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      
+    ]
   }
+  
 ];
 
 export default routes;
